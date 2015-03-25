7b1172824aaddb98c2ed8339506f6c7583a155e2
Sathvik Swamy <sathvik_1@hotmail.com>
2015-03-24 21:01:05 -0400

    Filling rest of the fields of the edit form

--- app/controllers/dashboard/housing/listings_controller.rb
+++ app/controllers/dashboard/housing/listings_controller.rb
@@ -4,6 +4,9 @@ class Dashboard::Housing::ListingsController < ApplicationController
   	if current_user.can_access_listing(params[:id])
 		find_listing_by_id = HousingListing.find(params[:id])
 		@housing_listing = find_listing_by_id
+
+		find_setting_by_id = HousingSetting.find_by(housing_listing_id: params[:id])
+		@housing_setting = find_setting_by_id
 	else
 		render plain: "No permission to access this listing", status: 404
 	end
